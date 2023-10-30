10c598755a915563ec7e5f022c4724fbe2d461ed
Sverre <rognar.m5@gmail.com>
2023-10-30 18:46:26 +0300

    dz14.0.8

--- sch.tf
+++ sch.tf
@@ -25,28 +25,27 @@ resource "yandex_compute_instance" "vm-1" {
   }
 
   boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu_image.id
-    }
+    disk_id =  yandex_compute_disk.default
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
+    subnet_id = "e9bohr7qvj70b390umrp"
     nat       = true
   }
 
   metadata = {
     user-data = "${file("./user.yml")}"
   }
+
+  allow_stopping_for_update {
+      true
+  }
+
 }
 
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
+resource "yandex_compute_disk" "default" {
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  image_id = data.yandex_compute_image.ubuntu_image.id
+  size = 15
 }
