718ba7cf4bee3043905fcbe2ee7b0772caf35638
Sverre <rognar.m5@gmail.com>
2023-11-01 15:24:47 +0300

    dz14.1.4

--- sch.tf
+++ sch.tf
@@ -16,16 +16,30 @@ provider "yandex" {
 data "yandex_compute_image" "ubuntu_image" {
   family = "ubuntu-2004-lts"
 }
+
+variable "paramsvm" {
+  description = "параметры машин"
+  type        = map(string)
+  default     = {
+    namevm1     = "lessonvm1",
+    cor1        = 2,
+    mem1        = 2,
+    namevm2     = "lessonvm2",
+    cor2        = 2,
+    mem2        = 2,
+  }
+}
 resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
+  name = var.paramsvm.namevm1
+  id = var.paramsvm.namevm1
   allow_stopping_for_update = true
   resources {
-    cores  = 2
-    memory = 2
+    cores  = var.paramsvm.cor1
+    memory = var.paramsvm.mem1
   }
 
   boot_disk {
-    disk_id =  yandex_compute_disk.default.id
+    disk_id =  yandex_compute_disk.hddvm1.id
   }
 
   network_interface {
@@ -42,7 +56,41 @@ resource "yandex_compute_instance" "vm-1" {
 
 }
 
-resource "yandex_compute_disk" "default" {
+resource "yandex_compute_disk" "hddvm1" {
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  image_id = data.yandex_compute_image.ubuntu_image.id
+  size = 15
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = var.paramsvm.namevm2
+  id = var.paramsvm.namevm2
+  allow_stopping_for_update = true
+  resources {
+    cores  = var.paramsvm.cor2
+    memory = var.paramsvm.mem2
+  }
+
+  boot_disk {
+    disk_id =  yandex_compute_disk.hddvm2.id
+  }
+
+  network_interface {
+    subnet_id = "e9bohr7qvj70b390umrp"
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./user.yml")}"
+  }
+  scheduling_policy {
+    preemptible = true 
+  }
+
+}
+
+resource "yandex_compute_disk" "hddvm2" {
   type     = "network-hdd"
   zone     = "ru-central1-a"
   image_id = data.yandex_compute_image.ubuntu_image.id
